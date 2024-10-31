05f796463b8ea76c06566e1728ff0e59f2e08f4f
cdivinity <140557169+cdivinity@users.noreply.github.com>
2024-10-31 01:08:27 -0500

    add validation

--- java.js
+++ java.js
@@ -5,3 +5,24 @@ function show() {
             document.getElementById('btnID')
                 .style.display = "none";
         }
+function validateForm() {
+var x = document.forms["gaming"]["message"].value;
+  if (x == "" || x == null) {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function validateForm2() {
+var x = document.forms["gaming"]["fname"].value;
+  if (x == "" || x == null) {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+function validateForm3() {
+var x = document.forms["gaming"]["email"].value;
+  if (x == "" || x == null) {
+    alert("Name must be filled out");
+    return false;
+  }
+}
